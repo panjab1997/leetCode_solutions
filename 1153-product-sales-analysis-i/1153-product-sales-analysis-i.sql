60e3c131c7b83596fbd4998ac025d4e1bc96293f
Panjab Singh <114966161+panjab1997@users.noreply.github.com>
2025-05-24 23:58:37 +0530

    Time: 1245 ms (59.26%), Space: 0B (100%) - LeetHub

--- 1153-product-sales-analysis-i/1153-product-sales-analysis-i.sql
+++ 1153-product-sales-analysis-i/1153-product-sales-analysis-i.sql
@@ -1,2 +1,2 @@
 # Write your MySQL query statement below
-select P.product_name, S.year ,S.price from Product P right join Sales S on P.product_id = S.product_id where S.year is not null ;
+select P.product_name, S.year ,S.price from Product P right join Sales S on P.product_id = S.product_id ;
